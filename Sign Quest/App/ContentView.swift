f2cb76ed1aa429e1446944a89b2641dafa3adfd5
EzraArya <ezrawijaya10@gmail.com>
2025-04-22 22:39:13 +0700

    chore: Set up app entry

--- Sign Quest/App/ContentView.swift
+++ Sign Quest/App/ContentView.swift
@@ -6,16 +6,12 @@
 //
 
 import SwiftUI
+import SignQuestUI
+import Onboarding
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        SQWelcomeView()
     }
 }
 
--- Sign Quest/App/Sign_QuestApp.swift
+++ Sign Quest/App/Sign_QuestApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import SignQuestUI
 
 @main
 struct Sign_QuestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+                    .applyBackground()
+            }
         }
     }
 }
