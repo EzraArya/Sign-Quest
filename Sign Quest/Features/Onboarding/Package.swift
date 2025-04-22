16f72ca6186a4ace51d6cbb58ff712824b981314
EzraArya <ezrawijaya10@gmail.com>
2025-04-22 22:44:10 +0700

    chore: update Onboarding package dependencies

--- Sign Quest/Features/Onboarding/Package.swift
+++ Sign Quest/Features/Onboarding/Package.swift
@@ -15,14 +15,15 @@ let package = Package(
             targets: ["Onboarding"]),
     ],
     dependencies: [
-        .package(path: "../SignQuestUI")
+        .package(path: "../SignQuestUI"),
+        .package(path: "../Authentication"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Onboarding",
-            dependencies: ["SignQuestUI"]
+            dependencies: ["SignQuestUI", "Authentication"]
         ),
         .testTarget(
             name: "OnboardingTests",
