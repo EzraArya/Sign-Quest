003b9e6d64db1792375477dc7a62deb16c3941b2
EzraArya <ezrawijaya10@gmail.com>
2025-04-22 22:43:22 +0700

    feat: add modifier function to view

---
+++ Sign Quest/SignQuestUI/SignQuestUI/Sources/SignQuestUI/Extension/View+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  View+Extension.swift
+//  SignQuestUI
+//
+//  Created by Ezra Arya Wijaya on 22/04/25.
+//
+
+import SwiftUI
+
+public extension View {
+    func applyBackground() -> some View {
+        self.modifier(SQBackgroundModifier())
+    }
+    
+    func applyTextFieldModifiers(
+            font: SQFont,
+            size: CGFloat,
+            style: SQTextFieldStyle,
+            cornerRadius: CGFloat,
+            padding: EdgeInsets,
+            keyboardType: UIKeyboardType
+        ) -> some View {
+            self.modifier(
+                SQTextFieldModifier(
+                    font: font,
+                    size: size,
+                    style: style,
+                    cornerRadius: cornerRadius,
+                    padding: padding,
+                    keyboardType: keyboardType
+            )
+        )
+    }
+}
