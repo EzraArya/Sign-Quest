030ac73ddd4f74751d82597f68c97b28b98377d9
EzraArya <ezrawijaya10@gmail.com>
2025-04-23 23:46:13 +0700

    chore: update entry

--- Sign Quest/App/ContentView.swift
+++ Sign Quest/App/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 import SignQuestUI
 import Onboarding
+import Dashboard
 
 struct ContentView: View {
+    @State private var isLoggedIn: Bool = true
+    
     var body: some View {
-        SQWelcomeView()
+        if isLoggedIn {
+            SQDashboardView()
+        } else {
+            SQWelcomeView()
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
