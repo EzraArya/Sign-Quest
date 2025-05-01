5298d1a09d0ba5b214f9fd9a810381d053e3f392
EzraArya <ezrawijaya10@gmail.com>
2025-05-01 21:06:12 +0700

    feat: Add SQHomeViewModel

---
+++ Sign Quest/Features/Home/Sources/Home/UI/ViewModel/SQHomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SQHomeViewModel.swift
+//  Home
+//
+//  Created by Ezra Arya Wijaya on 01/05/25.
+//
+
+import SwiftUI
+import Combine
+
+class SQHomeViewModel: ObservableObject {
+    @Published public var user: String = "User"
+}
