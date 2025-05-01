237a21d691136f166a4940a2991240406cf7bc21
EzraArya <ezrawijaya10@gmail.com>
2025-05-01 22:49:57 +0700

    feat: Add SignQuestModels Dependency

--- Sign Quest/Features/Home/Package.swift
+++ Sign Quest/Features/Home/Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         .package(path: "../SignQuestUI"),
         .package(path: "../SignQuestInterfaces"),
+        .package(path: "../SignQuestModels")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Home",
-            dependencies: ["SignQuestUI", "SignQuestInterfaces"],
+            dependencies: ["SignQuestUI", "SignQuestInterfaces", "SignQuestModels"],
         ),
         .testTarget(
             name: "HomeTests",
