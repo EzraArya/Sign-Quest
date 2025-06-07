a73d54d85f77f102c5da2d35421afd9b7bce7cc3
EzraArya <ezrawijaya10@gmail.com>
2025-06-07 19:43:31 +0700

    feat: Update Auth Network Service

--- Sign Quest/Features/Authentication/Sources/Authentication/Service/Network/SQAuthenticationNetworkService.swift
+++ Sign Quest/Features/Authentication/Sources/Authentication/Service/Network/SQAuthenticationNetworkService.swift
@@ -10,27 +10,21 @@ import FirebaseAuth
 import FirebaseFirestore
 
 protocol SQAuthenticationNetworkServiceProtocol {
-    func login(email: String, password: String) async throws -> User
-    func register(user: SQUser, password: String) async throws -> SQUser
+    func login(email: String, password: String) async throws
+    func register(user: SQUser, password: String) async throws
 }
 
 struct SQAuthenticationNetworkService: SQAuthenticationNetworkServiceProtocol {
     
     private let db = Firestore.firestore()
     
-    func login(email: String, password: String) async throws -> User {
-        let authResult = try await Auth.auth().signIn(withEmail: email, password: password)
-        return authResult.user
+    func login(email: String, password: String) async throws {
+        try await Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    func register(user: SQUser, password: String) async throws -> SQUser {
+    func register(user: SQUser, password: String) async throws {
         let authResult = try await Auth.auth().createUser(withEmail: user.email, password: password)
         let uid = authResult.user.uid
-        
-        var userToSave = user
-        userToSave.id = uid
-
-        try db.collection("users").document(uid).setData(from: userToSave)
-        return userToSave
+        try db.collection("users").document(uid).setData(from: user)
     }
 }
