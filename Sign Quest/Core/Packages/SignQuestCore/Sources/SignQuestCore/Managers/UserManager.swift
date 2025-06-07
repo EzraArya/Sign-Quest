020b35c204650d4cf306d32817938428b6e6c3e5
EzraArya <ezrawijaya10@gmail.com>
2025-06-08 00:59:58 +0700

    chore: Update listener

--- Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Managers/UserManager.swift
+++ Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Managers/UserManager.swift
@@ -11,12 +11,14 @@ import FirebaseFirestore
 import Combine
 import SignQuestModels
 
+@MainActor
 public class UserManager: ObservableObject {
     
     @Published public var authUser: FirebaseAuth.User?
     @Published public var firestoreUser: SQUser?
     
     private var authStateHandler: AuthStateDidChangeListenerHandle?
+    private var firestoreListener: ListenerRegistration?
     
     public init() {
         addAuthStateListener()
@@ -30,53 +32,64 @@ public class UserManager: ObservableObject {
             self.authUser = user
             
             if let user = user {
-                self.fetchFirestoreUser(uid: user.uid)
+                self.setupFirestoreListener(for: user.uid)
             } else {
+                self.firestoreListener?.remove()
+                self.firestoreListener = nil
                 self.firestoreUser = nil
             }
         }
     }
     
-    private func fetchFirestoreUser(uid: String) {
+    private func setupFirestoreListener(for uid: String) {
         let db = Firestore.firestore()
-        db.collection("users").document(uid).getDocument { [weak self] (document, error) in
+        
+        self.firestoreListener?.remove()
+        
+        self.firestoreListener = db.collection("users").document(uid).addSnapshotListener { [weak self] document, error in
             guard let self = self else { return }
-            if let document = document, document.exists {
-                do {
-                    self.firestoreUser = try document.data(as: SQUser.self)
-                } catch {
-                    print("Error decoding user data: \(error)")
+            
+            Task { @MainActor in
+                if let error = error {
+                    print("Error listening to user document: \(error)")
+                    self.firestoreUser = nil
+                    return
                 }
-            } else {
-                print("User document does not exist")
-                self.firestoreUser = nil
+                
+                if let document = document, document.exists {
+                    do {
+                        self.firestoreUser = try document.data(as: SQUser.self)
+                    } catch {
+                        print("Error decoding user data: \(error)")
+                    }
+                } else {
+                    print("User document does not exist")
+                    self.firestoreUser = nil
+                }
             }
         }
     }
     
     public func signOut() {
+        if let authStateHandler = authStateHandler {
+            Auth.auth().removeStateDidChangeListener(authStateHandler)
+            self.authStateHandler = nil
+        }
+        
+        self.firestoreListener?.remove()
+        self.firestoreListener = nil
         try? Auth.auth().signOut()
     }
     
-    public func deleteAccount() {
+    public func deleteAccount() async {
         guard let user = authUser else { return }
-        
         let db = Firestore.firestore()
-        db.collection("users").document(user.uid).delete { [weak self] error in
-            if let error = error {
-                print("Error deleting user document: \(error)")
-            } else {
-                user.delete()
-                self?.authUser = nil
-                self?.firestoreUser = nil
-            }
-        }
-    }
-    
-    deinit {
-        if let authStateHandler = authStateHandler {
-            Auth.auth().removeStateDidChangeListener(authStateHandler)
+        
+        do {
+            try await db.collection("users").document(user.uid).delete()
+            try await user.delete()
+        } catch {
+            print("Error deleting account: \(error)")
         }
     }
 }
-
