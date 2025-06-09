22e257586f05a00ac5f5dcc7bca168e6c4b50a51
EzraArya <ezrawijaya10@gmail.com>
2025-06-09 21:01:59 +0700

    feat: Add SQEditProfilePictureView

---
+++ Sign Quest/Features/Profile/Sources/Profile/UI/View/Edit Profile Picture/SQEditProfilePictureView.swift
@@ -0,0 +1,21 @@
+//
+//  SQEditProfilePictureView.swift
+//  Profile
+//
+//  Created by Ezra Arya Wijaya on 08/06/25.
+//
+
+import SwiftUI
+
+public struct SQEditProfilePictureView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SQEditProfilePictureView()
+}
