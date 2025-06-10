327c993de724c7e88032cd9045cbf4d7d08ec852
EzraArya <ezrawijaya10@gmail.com>
2025-06-10 19:04:35 +0700

    feat: Implement SQEditProfilePictureViewModel

--- Sign Quest/Features/Profile/Sources/Profile/UI/ViewModel/Edit Profile Picture/SQEditProfilePictureViewModel.swift
+++ Sign Quest/Features/Profile/Sources/Profile/UI/ViewModel/Edit Profile Picture/SQEditProfilePictureViewModel.swift
@@ -5,3 +5,43 @@
 //  Created by Ezra Arya Wijaya on 08/06/25.
 //
 
+import SwiftUI
+import SignQuestCore
+
+@MainActor
+class SQEditProfilePictureViewModel: ObservableObject {
+    @Published var profilePicture: UIImage? = nil
+    
+    private var userManager: UserManager?
+    private var coordinator: SQProfileCoordinator?
+    private let networkService: SQProfileNetworkServiceProtocol
+    
+    init(networkService: SQProfileNetworkServiceProtocol = SQProfileNetworkService()) {
+        self.networkService = networkService
+    }
+    
+    func link(userManager: UserManager, coordinator: SQProfileCoordinator) {
+        self.userManager = userManager
+        self.coordinator = coordinator
+    }
+    
+    func updateProfilePicture(_ image: UIImage) async {
+        guard let userId = userManager?.firestoreUser?.id, let profilePicture = profilePicture else {
+            return
+        }
+        
+        Task {
+            try await networkService.uploadProfilePicture(userId: userId, image: profilePicture)
+        }
+    }
+    
+    func navigateBack() {
+        coordinator?.pop()
+    }
+}
+
+extension SQEditProfilePictureViewModel {
+    var userProfilePicture: String? {
+        return userManager?.firestoreUser?.image
+    }
+}
