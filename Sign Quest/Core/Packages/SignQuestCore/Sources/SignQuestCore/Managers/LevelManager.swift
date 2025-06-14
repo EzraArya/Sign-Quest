9a789c0734d5b016a58594eb2cf1295c73b8af32
EzraArya <ezrawijaya10@gmail.com>
2025-06-14 23:45:33 +0700

    feat: Create LevelManager

---
+++ Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Managers/LevelManager.swift
@@ -0,0 +1,15 @@
+//
+//  LevelManager.swift
+//  SignQuestCore
+//
+//  Created by Ezra Arya Wijaya on 13/06/25.
+//
+
+import SignQuestModels
+
+public final class LevelManager: @unchecked Sendable {
+    static let shared = LevelManager()
+    
+    private init() {}
+    
+}
