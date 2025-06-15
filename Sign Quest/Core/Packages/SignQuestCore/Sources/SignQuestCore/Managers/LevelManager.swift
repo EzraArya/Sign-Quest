c3c2bce6fbe03a797c4201738773c2651cabb014
EzraArya <ezrawijaya10@gmail.com>
2025-06-15 21:39:47 +0700

    feat: Add Network API function

--- Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Managers/LevelManager.swift
+++ Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Managers/LevelManager.swift
@@ -9,7 +9,25 @@ import SignQuestModels
 
 public final class LevelManager: @unchecked Sendable {
     static let shared = LevelManager()
+    private let networkService: LevelServiceProtocol
     
-    private init() {}
+    private init(networkService: LevelServiceProtocol = LevelService()) {
+        self.networkService = networkService
+    }
     
+    private func fetchLevel(by id: String) async throws -> SQLevel {
+        return try await networkService.fetchLevel(levelId: id)
+    }
+    
+    private func userLevelData(for userId: String, by levelId: String) async throws -> SQUserLevelData {
+        return try await networkService.fetchUserLevelData(userId: userId, levelId: levelId)
+    }
+    
+    private func fetchQuestions(for levelId: String) async throws -> [SQQuestion] {
+        return try await networkService.fetchQuestions(for: levelId)
+    }
+    
+    private func updateUserLevelData(_ data: SQUserLevelData, for userId: String, levelId: String) async throws {
+        try await networkService.updateUserLevelData(for: userId, levelId: levelId, userLevelData: data)
+    }
 }
