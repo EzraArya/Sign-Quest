df772abe902dd3cd4a8660e44fca49ea0c07dcaa
EzraArya <ezrawijaya10@gmail.com>
2025-06-15 21:39:39 +0700

    chore: update return value

--- Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Service/Network Service/LevelService.swift
+++ Sign Quest/Core/Packages/SignQuestCore/Sources/SignQuestCore/Service/Network Service/LevelService.swift
@@ -9,19 +9,19 @@ import SignQuestModels
 import FirebaseFirestore
 
 protocol LevelServiceProtocol {
-    func fetchLevel(levelId: String) async throws -> SQLevel?
-    func fetchUserLevelData(userId: String, levelId: String) async throws-> SQUserLevelData?
+    func fetchLevel(levelId: String) async throws -> SQLevel
+    func fetchUserLevelData(userId: String, levelId: String) async throws-> SQUserLevelData
     func updateUserLevelData(for userId: String, levelId: String, userLevelData: SQUserLevelData) async throws
-    func fetchQuestions(for levelId: String) async throws -> [SQQuestion]?
+    func fetchQuestions(for levelId: String) async throws -> [SQQuestion]
 }
 
 struct LevelService: LevelServiceProtocol {
-    func fetchLevel(levelId: String) async throws -> SQLevel? {
+    func fetchLevel(levelId: String) async throws -> SQLevel {
         let db = Firestore.firestore()
         return try await db.collection("levels").document(levelId).getDocument(as: SQLevel.self)
     }
     
-    func fetchUserLevelData(userId: String, levelId: String) async throws-> SQUserLevelData? {
+    func fetchUserLevelData(userId: String, levelId: String) async throws-> SQUserLevelData {
         let db = Firestore.firestore()
         return try await db.collection("users").document(userId).collection("levelData").document(levelId).getDocument(as: SQUserLevelData.self)
     }
@@ -31,7 +31,7 @@ struct LevelService: LevelServiceProtocol {
         try db.collection("users").document(userId).collection("levelData").document(levelId).setData(from: userLevelData)
     }
     
-    func fetchQuestions(for levelId: String) async throws -> [SQQuestion]? {
+    func fetchQuestions(for levelId: String) async throws -> [SQQuestion] {
         let db = Firestore.firestore()
         return try await db.collection("questions").whereField("levelId", isEqualTo: levelId).getDocuments().documents.compactMap { document in
             try? document.data(as: SQQuestion.self)
