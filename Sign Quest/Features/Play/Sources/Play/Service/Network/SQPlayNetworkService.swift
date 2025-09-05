2d57aed3fd60abf1384355e93bc733b7fff86a52
EzraArya <ezrawijaya10@gmail.com>
2025-09-05 21:13:39 +0700

    feat: Add SQPlayNetworkService

--- Sign Quest/Features/Play/Sources/Play/Service/Network/SQPlayNetworkService.swift
+++ Sign Quest/Features/Play/Sources/Play/Service/Network/SQPlayNetworkService.swift
@@ -6,10 +6,38 @@
 //
 
 import SignQuestModels
+import Firebase
 import Foundation
 
-struct SQPlayNetworkService {
-    func fetchLevel(levelId: String) async -> SQLevel? {
-        return nil
+protocol SQPlayNetworkServiceProtocol {
+    func fetchLevel(levelId: String) async throws -> SQLevel
+    func fetchQuestions(levelId: String) async throws -> [SQQuestion]
+    func updateUserLevelData(userId: String, levelId: String, userLevelData: SQUserLevelData) async throws
+}
+
+struct SQPlayNetworkService: SQPlayNetworkServiceProtocol, Sendable {
+    func fetchLevel(levelId: String) async throws -> SQLevel {
+        let db = Firestore.firestore()
+        let snapshot = try await db.collection("levels")
+            .document(levelId)
+            .getDocument()
+        return try snapshot.data(as: SQLevel.self)
+    }
+    
+    func fetchQuestions(levelId: String) async throws -> [SQQuestion] {
+        let db = Firestore.firestore()
+        let snapshot = try await db.collection("questions")
+            .whereField("levelId", isEqualTo: levelId)
+            .getDocuments()
+        return try snapshot.documents.compactMap { try $0.data(as: SQQuestion.self) }
+    }
+    
+    func updateUserLevelData(userId: String, levelId: String, userLevelData: SQUserLevelData) async throws {
+        let db = Firestore.firestore()
+        let docRef = db.collection("users")
+            .document(userId)
+            .collection("levelData").document(levelId)
+        
+        try docRef.setData(from: userLevelData, merge: true)
     }
 }
